471e1d825eec715c0b582019734bd6fe00a0c4ae
Venkata Ramanarao Ponnapalli <Venkata_Ramanarao_Ponnapalli@gap.com>
2019-05-21 12:02:58 -0700

    updated code recently

--- src/main/java/com/demo/samplekafka/SampleConsumer.java
+++ src/main/java/com/demo/samplekafka/SampleConsumer.java
@@ -1,10 +1,45 @@
 package com.demo.samplekafka;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 public class SampleConsumer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		String topicName = "events" ;
+		String groupName = "eventsTopicGroup";
 
+		Properties props = new Properties();
+
+		props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+		props.put("group.id", groupName);
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+		KafkaConsumer<String, String> consumer = null;
+		
+		
+		try {
+			consumer = new KafkaConsumer<>(props);
+			consumer.subscribe(Collections.singletonList(topicName));
+			System.out.println(" message is published in consumer");
+			@SuppressWarnings("deprecation")
+			ConsumerRecords<String, String> records=consumer.poll(1000);
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.println(" Received message is" + record.key() + " value is " + record.value());
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+		consumer.close();
+	}
 }
